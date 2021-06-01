2be897c9fdd159083e79b0b9aa5236dacf4c6e6c
unknown <abderrahmane.benchekroun@gmail.com>
2021-06-01 11:25:19 +0200

    Spark_SQL

--- src/main/scala/TpCore2.scala
+++ src/main/scala/TpCore2.scala
@@ -6,7 +6,7 @@ object TpCore2 {
     val sc = new SparkContext(conf)
     val rdd = sc.textFile("file:///csv/people12.csv") //Line 1
     val splitRdd = rdd.filter(line => !line.contains ("year")).map(line => line.split(",")) //Line 2
-    val fieldRdd = splitRdd.map(f => (f(1),f(3).toInt)) //Line 3
+    val fieldRdd = splitRdd.map(f => (f(1),f(3).trim.toInt)) //Line 3
     val namesCount=fieldRdd.reduceByKey((v1,v2) => v1 + v2) //Line 4
     namesCount.foreach(println) //Line 5
 
